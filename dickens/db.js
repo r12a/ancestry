bfe08ab14671ff9d03bb30eb9311f24ccc23be58
r12a <ishida@w3.org>
2017-05-08 20:21:56 +0100

    dickens/db.js, fixed order

--- dickens/db.js
+++ dickens/db.js
@@ -1,54 +1,5 @@
 db = {
 
-dickens_john_1785:{
-g:"John", k:"", f:"Dickens", bdate:"21 Aug", b:"1785", ddate:"31 Mar", d:"1851",
-bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
-father:"William Dickens", mother:"Elizabeth Bal", occ:"Clerk",
-fg:[["1809", "barrow_elizabeth_culliford_1789", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
-},
-
-dickens_alfred_allen_1813:{
-g:"Alfred Allen", k:"", f:"Dickens", bdate:"", b:"1813", ddate:"", d:"1813",
-bplace:"", dplace:"", thumb:false, male:true, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", cstatus:"Died as infant",
-},
-
-dickens_frederick_1820:{
-g:"Frederick", k:"", f:"Dickens", bdate:"", b:"1820", ddate:"", d:"1868",
-bplace:"", dplace:"", thumb:false, male:true, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"",
-},
-
-dickens_alfred_lamert_1822:{
-g:"Alfred Lamert", k:"", f:"Dickens", bdate:"", b:"1822", ddate:"", d:"1860",
-bplace:"", dplace:"", thumb:false, male:true, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Railway engineer",
-},
-
-dickens_augustus_1827:{
-g:"Augustus", k:"", f:"Dickens", bdate:"", b:"1827", ddate:"", d:"1866",
-bplace:"", dplace:"", thumb:false, male:true, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Railway engineer",
-},
-
-dickens_frances_elizabeth_1810:{
-g:"Frances Elizabeth", k:"Fanny", f:"Dickens", bdate:"", b:"1810", ddate:"", d:"1848",
-bplace:"", dplace:"", thumb:false, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", m:[],
-},
-
-dickens_harriet_1819:{
-g:"Harriet", k:"", f:"Dickens", bdate:"", b:"1819", ddate:"", d:"1824",
-bplace:"", dplace:"", thumb:false, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", cstatus:"Died as child",
-},
-
-dickens_letitia_1816:{
-g:"Letitia", k:"", f:"Dickens", bdate:"", b:"1816", ddate:"", d:"1893",
-bplace:"", dplace:"", thumb:false, p:false,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", m:["Austin"],
-},
-
 barrow_elizabeth_culliford_1789:{
 g:"Elizabeth Culliford", k:"Elizabeth", f:"Barrow", bdate:"21 Dec", b:"1789", ddate:"13 Sep", d:"1863",
 bplace:"", dplace:"London", thumb:true, p:false,
@@ -56,56 +7,17 @@ father:"Charles Barrow", mother:"Mary Culliford", occ:"", m:['Dickens'],
 fg:[["1836", "dickens_john_1785", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
 },
 
-dickens_charles_john_huffam_1812:{
-g:"Charles John Huffam", k:"Charles", f:"Dickens", bdate:"7 Feb", b:"1812", ddate:"9 Jun", d:"1870",
-bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
-father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Writer",
-fg:[["1836", "hogarth_catherine_thompson_1815", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845", "dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]]
+collins_charles_allston_1828:{
+g:"Charles Allston", k:"Charles", f:"Collins", bdate:"25 Jan", b:"1828", ddate:"9 Apr", d:"1873",
+bplace:"Hampstead", dplace:"London", thumb:true, male:true, p:false,
+father:"William Collins", mother:"", occ:"Artist, writer", cstatus:"No children",
+fg:[["1860", "dickens_catherine_elizabeth_macready_1839"]]
 },
 
-hogarth_catherine_thompson_1815:{
-g:"Catherine Thompson", k:"Kate", f:"Hogarth", bdate:"19 May", b:"1815", ddate:"22 Nov", d:"1879",
-bplace:"Edinburgh", dplace:"London", thumb:true, p:false,
-father:"George Hogarth", mother:"", occ:"", m:['Dickens'],
-fg:[["1836", "dickens_charles_john_huffam_1812", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845","dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]]
-},
-
-dickens_charles_culliford_boz_1837:{
-g:"Charles Culliford Boz", k:"Charley", f:"Dickens", bdate:"6 Jan", b:"1837", ddate:"20 Jul", d:"1896",
-bplace:"Holborn", dplace:"Fulham", thumb:true, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Writer, editor",
-fg:[["1861", "Elizabeth Matilda Moule Evans"]]
-},
-
-dickens_sydney_smith_haldimand_1847:{
-g:"Sydney Smith Haldimand", k:"", f:"Dickens", bdate:"18 Apr", b:"1847", ddate:"2 May", d:"1872",
-bplace:"", dplace:"Indian Ocean", thumb:false, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Naval lieutenant", cstatus:"Died single"
-},
-
-dickens_edward_bulwer_lytton_1852:{
-g:"Edward Bulwer Lytton", k:"Edward", f:"Dickens", bdate:"13 Mar", b:"1852", ddate:"23 Jan", d:"1902",
-bplace:"", dplace:"Moree, NSW", thumb:false, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Politician", cstatus:"Died single"
-},
-
-dickens_henry_fielding_1849:{
-g:"Henry Fielding", k:"", f:"Dickens", bdate:"16 Jan", b:"1849", ddate:"21 Dec", d:"1933",
-bplace:"London", dplace:"London", thumb:true, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Common Serjeant of London", 
-fg:[["1876", "Marie Roche"]]
-},
-
-dickens_walter_savage_landor_1841:{
-g:"Walter Savage Landor", k:"Walter", f:"Dickens", bdate:"8 Feb", b:"1841", ddate:"31 Dec", d:"1863",
-bplace:"London", dplace:"Calcutta, IN", thumb:false, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Mountie", cstatus:"Died single"
-},
-
-dickens_francis_jeffrey_1844:{
-g:"Francis Jeffrey", k:"Frank", f:"Dickens", bdate:"15 jan", b:"1844", ddate:"11 Jun", d:"1886",
-bplace:"London", dplace:"Moline, IL", thumb:false, male:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Indian Army Officer", cstatus:"Died single"
+dickens_alfred_allen_1813:{
+g:"Alfred Allen", k:"", f:"Dickens", bdate:"", b:"1813", ddate:"", d:"1813",
+bplace:"", dplace:"", thumb:false, male:true, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", cstatus:"Died as infant"
 },
 
 dickens_alfred_dorsay_tennison_1845:{
@@ -115,16 +27,16 @@ father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_18
 fg:[["~1874", "Augusta Jessie Devlin", "Kathleen Mary", "Violet Georgina"]]
 },
 
-dickens_mary_1838:{
-g:"Mary", k:"Mamie", f:"Dickens", bdate:"6 Mar", b:"1838", ddate:"23 Jul", d:"1896",
-bplace:"London", dplace:"Farnham Royal", thumb:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Author", m:[], cstatus:"Died single",
+dickens_alfred_lamert_1822:{
+g:"Alfred Lamert", k:"", f:"Dickens", bdate:"", b:"1822", ddate:"", d:"1860",
+bplace:"", dplace:"", thumb:false, male:true, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Railway engineer"
 },
 
-dickens_mary_1838:{
-g:"Mary", k:"Mamie", f:"Dickens", bdate:"6 Mar", b:"1838", ddate:"23 Jul", d:"1896",
-bplace:"London", dplace:"Farnham Royal", thumb:true, p:false,
-father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Author", m:[], cstatus:"Died single",
+dickens_augustus_1827:{
+g:"Augustus", k:"", f:"Dickens", bdate:"", b:"1827", ddate:"", d:"1866",
+bplace:"", dplace:"", thumb:false, male:true, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Railway engineer"
 },
 
 dickens_catherine_elizabeth_macready_1839:{
@@ -135,32 +47,112 @@ fg:[["1860", "collins_charles_allston_1828"],
     ["1874", "perugini_charles_edward_1839", "perugini_leonard_ralph_dickens_1876"]]
 },
 
+dickens_charles_culliford_boz_1837:{
+g:"Charles Culliford Boz", k:"Charley", f:"Dickens", bdate:"6 Jan", b:"1837", ddate:"20 Jul", d:"1896",
+bplace:"Holborn", dplace:"Fulham", thumb:true, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Writer, editor",
+fg:[["1861", "Elizabeth Matilda Moule Evans"]]
+},
+
+dickens_charles_john_huffam_1812:{
+g:"Charles John Huffam", k:"Charles", f:"Dickens", bdate:"7 Feb", b:"1812", ddate:"9 Jun", d:"1870",
+bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Writer",
+fg:[["1836", "hogarth_catherine_thompson_1815", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845", "dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]]
+},
+
 dickens_dora_annie_1850:{
 g:"Dora Annie", k:"", f:"Dickens", bdate:"16 Aug", b:"1850", ddate:"14 Apr", d:"1851",
 bplace:"London", dplace:"London", thumb:false, p:true,
 father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"", m:[], cstatus:"Died as infant"
 },
 
-collins_charles_allston_1828:{
-g:"Charles Allston", k:"Charles", f:"Collins", bdate:"25 Jan", b:"1828", ddate:"9 Apr", d:"1873",
-bplace:"Hampstead", dplace:"London", thumb:true, male:true, p:false,
-father:"William Collins", mother:"", occ:"Artist, writer", cstatus:"No children",
-fg:[["1860", "dickens_catherine_elizabeth_macready_1839"]]
+dickens_edward_bulwer_lytton_1852:{
+g:"Edward Bulwer Lytton", k:"Edward", f:"Dickens", bdate:"13 Mar", b:"1852", ddate:"23 Jan", d:"1902",
+bplace:"", dplace:"Moree, NSW", thumb:false, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Politician", cstatus:"Died single"
+},
+
+dickens_frances_elizabeth_1810:{
+g:"Frances Elizabeth", k:"Fanny", f:"Dickens", bdate:"", b:"1810", ddate:"", d:"1848",
+bplace:"", dplace:"", thumb:false, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", m:[]
+},
+
+dickens_francis_jeffrey_1844:{
+g:"Francis Jeffrey", k:"Frank", f:"Dickens", bdate:"15 jan", b:"1844", ddate:"11 Jun", d:"1886",
+bplace:"London", dplace:"Moline, IL", thumb:false, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Indian Army Officer", cstatus:"Died single"
+},
+
+dickens_frederick_1820:{
+g:"Frederick", k:"", f:"Dickens", bdate:"", b:"1820", ddate:"", d:"1868",
+bplace:"", dplace:"", thumb:false, male:true, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:""
+},
+
+dickens_harriet_1819:{
+g:"Harriet", k:"", f:"Dickens", bdate:"", b:"1819", ddate:"", d:"1824",
+bplace:"", dplace:"", thumb:false, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", cstatus:"Died as child"
+},
+
+dickens_henry_fielding_1849:{
+g:"Henry Fielding", k:"", f:"Dickens", bdate:"16 Jan", b:"1849", ddate:"21 Dec", d:"1933",
+bplace:"London", dplace:"London", thumb:true, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Common Serjeant of London",
+fg:[["1876", "Marie Roche"]]
+},
+
+dickens_john_1785:{
+g:"John", k:"", f:"Dickens", bdate:"21 Aug", b:"1785", ddate:"31 Mar", d:"1851",
+bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
+father:"William Dickens", mother:"Elizabeth Bal", occ:"Clerk",
+fg:[["1809", "barrow_elizabeth_culliford_1789", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
+},
+
+dickens_letitia_1816:{
+g:"Letitia", k:"", f:"Dickens", bdate:"", b:"1816", ddate:"", d:"1893",
+bplace:"", dplace:"", thumb:false, p:false,
+father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"", m:['Austin']
+},
+
+dickens_mary_1838:{
+g:"Mary", k:"Mamie", f:"Dickens", bdate:"6 Mar", b:"1838", ddate:"23 Jul", d:"1896",
+bplace:"London", dplace:"Farnham Royal", thumb:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Author", m:[], cstatus:"Died single"
+},
+
+dickens_sydney_smith_haldimand_1847:{
+g:"Sydney Smith Haldimand", k:"", f:"Dickens", bdate:"18 Apr", b:"1847", ddate:"2 May", d:"1872",
+bplace:"", dplace:"Indian Ocean", thumb:false, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Naval lieutenant", cstatus:"Died single"
+},
+
+dickens_walter_savage_landor_1841:{
+g:"Walter Savage Landor", k:"Walter", f:"Dickens", bdate:"8 Feb", b:"1841", ddate:"31 Dec", d:"1863",
+bplace:"London", dplace:"Calcutta, IN", thumb:false, male:true, p:false,
+father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Mountie", cstatus:"Died single"
+},
+
+hogarth_catherine_thompson_1815:{
+g:"Catherine Thompson", k:"Kate", f:"Hogarth", bdate:"19 May", b:"1815", ddate:"22 Nov", d:"1879",
+bplace:"Edinburgh", dplace:"London", thumb:true, p:false,
+father:"George Hogarth", mother:"", occ:"", m:['Dickens'],
+fg:[["1836", "dickens_charles_john_huffam_1812", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845", "dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]]
 },
 
 perugini_charles_edward_1839:{
 g:"Charles Edward", k:"", f:"Perugini", bdate:"1 Sep", b:"1839", ddate:"22 Dec", d:"1918",
 bplace:"Naples, IT", dplace:"London", thumb:true, male:true, p:false,
-father:"", mother:"", occ:"Artist, writer", 
+father:"", mother:"", occ:"Artist, writer",
 fg:[["1874", "dickens_catherine_elizabeth_macready_1839", "perugini_leonard_ralph_dickens_1876"]]
 },
 
 perugini_leonard_ralph_dickens_1876:{
 g:"Leonard Ralph Dickens", k:"", f:"Perugini", bdate:"", b:"1876", ddate:"24 Jul", d:"1876",
 bplace:"", dplace:"", thumb:false, male:true, p:false,
-father:"perugini_charles_edward_1839", mother:"dickens_catherine_elizabeth_macready_1839", occ:"", cstatus:"Died as infant",
+father:"perugini_charles_edward_1839", mother:"dickens_catherine_elizabeth_macready_1839", occ:"", cstatus:"Died as infant"
 },
-
-
 
 }
