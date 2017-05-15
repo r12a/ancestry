f18db330ff09f4cf184d4bb00acd095d2140913f
r12a <ishida@w3.org>
2017-05-15 13:15:17 +0100

    history.js, new

---
+++ history.js
@@ -0,0 +1,60 @@
+var h = {}
+
+h[1766] = "The United States Declaration of Independence is ratified on July 4, 1776. It declares that the Americans were no longer under British rule. Instead, the thirteen British colonies come together to become a new country."
+	
+h[1792] = "The <a href='https://en.wikipedia.org/wiki/French_Revolution' target='_blank'>French Revolution</a> covered the years 1789-1799, in which the monarchy was overthrown and radical restructuring was forced upon the Roman Catholic Church. The French Republic is declared on 21 Sep, and Louis XVI is executed on 21 Jan 1793. The reign of terror lasts 1793-4."
+	
+h[1833] = "The <a href='https://en.wikipedia.org/wiki/Slavery_Abolition_Act_1833' target='_blank'>Slavery Abolition Act</a> is a result of a movement growing since the early 1780s, and abolishes slavery throughout the British Empire (with a few exceptions that were dealt with later). Slaves over 6 years old become 'apprentices' until 1838 or 1840, when their servitude is finally abolished."
+	
+h[1806] = "After 20 years at the helm <a href='https://en.wikipedia.org/wiki/William_Pitt_the_Younger' target='_blank'>Prime Minister William Pitt the Younger</a> (28 May 1759 – 23 January 1806) dies. For most of that time he was considered an inspiring figure, and in England 'there was a sense of loss almost as if the sun had fallen from the sky'."
+	
+h[1815] = "At the <a href='https://en.wikipedia.org/wiki/Battle_of_Waterloo' target='_blank'>Battle of Waterloo</a>, which takes place in Belgium on June 18, the Duke of Wellington secures the final defeat of French military leader and emperor Napoleon Bonaparte (1769-1821), who conquered much of continental Europe in the early 19th century."
+	
+h[1834] = "The <a href='https://en.wikipedia.org/wiki/Tolpuddle_Martyrs' target='_blank'>Tolpuddle Martyrs</a> are sentenced to deportation for taking secret oaths, such is the reactionary government's fear of revolution. It causes a public outcry."
+	
+h[1837] = "<a target='_blank' href='https://en.wikipedia.org/wiki/Queen_Victoria'>Queen Victoria</a> acceeds to the throne when she was only 18 years of age on June 20."
+	
+h[1838] = "<a target='_blank' href='https://en.wikipedia.org/wiki/Queen_Victoria'>Queen Victoria</a> is crowned on 28 June.</p><p>Chartist Thomas Attwood, founder of the Birmingham Political Union, presents the 'People's Charter' to parliament: 6 demands for political reform."
+	
+h[1840] = "Rowland Hill implements a uniform <a href='https://en.wikipedia.org/wiki/Penny_Post' target='_blank'>post rate of one penny</a>, irrespective of distance, to reform Britain's postal system which had been expensive, complex, open to abuse and causing widespread discontent. In the following decade the volume of letters sent in Britain increases five-fold to almost 350 million a year."
+	
+h[1845] = "Withdrawal of support for rates drives Britain into the 'Hungry Forties'. Unemployment in northern textile towns is so bad that the poor are living on money raised by local citizens and the workhouses overflowed. Indirect taxation adds to the pressure.</p><p>In September, the potato crop which had previously provided approximately 60% of the nation's food needs, <a href='https://en.wikipedia.org/wiki/Great_Famine_(Ireland)' target='_blank'>begs to rot all over Ireland</a>. The potato blight strikes again the following year. What began as a natural catastrophe is exacerbated by the actions and inactions of the British government. It is estimated that about a million people die during the four-year famine, and that between 1845 and 1855 another million emigrate, most to Britain and North America."
+	
+h[1851] = "In addition to the population census of England and Wales, a census is taken of places of worship. This reveals that around 80% of Welsh worshipers go to Nonconformist chapels and only 20% to the established church. Awareness of this numerical superiority greatly encourages the demand for the disestablishment of the Church of England in Wales. Partly due to the opposition of the Conservative party and the House of Lords, this did not take place until 1914."
+	
+h[1854] = "British ships sail from Constantinople to Crimea, planning to destroy the Russian military installation at Sebastopol, and prevent Russian interference in Turkey. The whole affair is a disaster, from incompetent supplies management to the charge of the Light Brigade. W H Russell of the London Times, the first war correspondent, makes the public aware of the catastrophic shambles and deference to absurd regulations."
+	
+h[1854] = "Total incompetence of British army in Crimea becomes a public scandal via W H Russell of the London Times, the first war correspondent."
+	
+h[1855] = "Before Peelite Aberdeen resigns, War Minister Sidney Herbert sends Florence Nightingale to Scutari, where she massively reduces mortality by adopting simple hygiene and adequate provisions. Palmerstone succeeds Aberdeen as PM in February. He listens to Nightingale, builds a railway and conditions improve vastly. Still, 40% of the 25,000 soldiers sent to the Crimea are killed. The French and Sardinians finally take Sebastopol."
+	
+h[1855] = "Florence Nightingale in Scutari. Whig Palmerstone becomes PM in February and acts on Nightingale's recommendations. Still, 40% of the 25,000 soldiers sent to the Crimea are killed, and the French and Sardinians have to take Sebastopol. Palmerstone is progressive internationally, but stops all domestic reform."
+	
+h[1856] = "The Treaty of Paris neutralises the Black Sea, holding back Russia for another 15 years. But despite the scandal and Royal Commission into the Crimean War, the British army resists adopting more than a few recommendations."
+	
+h[1861] = "The <a href='https://en.wikipedia.org/wiki/American_Civil_War' target='_blank'>American Civil War</a> starts on 12 Apr 1861 and concluded on 9 May 1865. The Union faces secessionists in eleven Southern states grouped together as the Confederate States of America."
+	
+h[1867] = "Disraeli's <a href='https://en.wikipedia.org/wiki/Reform_Act_1867' target='_blank'>Reform Act</a> permits large extensions to the franchise, virtually doubling the electorate, enabling one-third of adult males in Britain and one-sixth in Ireland to vote in parliamentary elections. In a few urban constituencies, working men are an electoral majority."
+	
+h[1880] = "Although WE Forster's act of 1870 had greatly expanded education opportunities, and an act passed in Benjamin Disraeli's government of 1876 had set up school attendance committees, significant gaps remained. AJ Mundella introduces a bill on behalf of William Gladstone's Liberal government which makes school attendance compulsory from ages five to 10. State expenditure on education, about £1.25 million a year in 1870, will rise to £4 million, and would reach £12 million by the end of Victoria's reign."
+	
+h[1883] = "The 1870 Married Women's Property Act changes the previous legal situation, in which all property automatically transferred to the control of a husband on marriage, granting limited protection to a married woman's property allowing women to retain up to £200 of their own wages or earnings. A further act in 1883 addresses criticisms by allowing women to acquire and retain any property deemed separate from that of their husband's, and provided legal protection."
+	
+h[1901] = "As queen-empress <a target='_blank' href='https://en.wikipedia.org/wiki/Queen_Victoria'>Victoria</a> had ruled over almost a quarter of the world's population. Although wilful and narrow-minded in some respects, she established firm precedents for a hard-working 'constitutional monarch', operating as a head of state above the fray of party politics. Her death on 22 Jan, coming so soon after the end of the 19th century, is truly the end of an era."
+	
+h[1908] = "New legislation on 27 Oct gives a weekly means-tested pension of a maximum of five shillings to all those aged over 70. Only about half a million people received the pension, and thus the significance of the legislation lies as much in the fact that it established a principle as in its immediate benefits."
+	
+h[1914] = "Belgium's ports are close to the British coast and German control of Belgium would be been seen as a serious threat to Britain. In the end, Britain refuses to ignore the events of 4 August, when Germany attacks France through Belgium. Within hours, Britain declares war on Germany. Conscription will be introduced on 27 Jan 1916."
+	
+h[1918] = "World War One ends at 11am on the eleventh day of the eleventh month. Germany signs an armistice that had been prepared by Britain and France."
+	
+h[1928] = "The Conservative government's 5th Reform Act on 7 May alters the 1918 Representation of the People Act, which had only allowed women over 30 who owned property to be enfranchised. The new act gives women the vote on the same terms as men."
+	
+h[1939] = "Germany invades Poland on 1 Sep. Since Great Britain had pledged military support to Poland if it were attacked by the Germans, it subsequently declares war against Germany on 3 Sep, hence beginning World War II."
+	
+h[1944] = "RA Butler, the progressive Conservative chancellor of the exchequer, creates universal free secondary education to the age of 15 in May, something people had campaigned for since the 19th century. There will be three types of schools - grammar, secondary modern and technical, entrance to which was determined by the '11 plus' examination."
+	
+h[1945] = "On 8 May, the Allies accept Germany’s surrender, about a week after Adolf Hitler had committed suicide, and World War II ends in Europe. Japan agrees in principle to unconditional surrender on 15 August 1945. Japan formally surrenders on 2 Sep, ending World War II throughout the rest of the world"
+	
+
+
