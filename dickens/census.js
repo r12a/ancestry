ccd4642dcf16029691056625e5edea88c480cdad
r12a <ishida@w3.org>
2022-02-19 14:54:25 +0000

    Charles & John reformatted

---
+++ dickens/census.js
@@ -0,0 +1,73 @@
+var censi = {
+
+"1841_dickens_charles_john_huffam_1812": {
+place: "Devonshire Terrace, St Marylebone, London",
+gps: ["1 Devonshire Terrace, London https://maps.google.com/maps?q=51.513611,-0.179810"],
+images: ["Census record url:records/1841_dickens_charles_catherine_c.jpg"],
+source: "HO107; Piece: 680; Book: 14; Civil Parish: St Marylebone; County: Middlesex; Enumeration District: 14; Folio: 12; Page: 18 http://search.ancestry.co.uk/cgi-bin/sse.dll?db=uki1841&indiv=try&h=7352195",
+cparish: "St Marylebone, srd Rectory, rd St Marylebone",
+
+head: "dickens_charles_john_huffam_1812;Chas Dickens;29;Gent;;not born in county",
+wife: "hogarth_catherine_thompson_1815;Catherine Dickens;26;;;born in Scotland, Ireland, or foriegn parts",
+children: [
+"dickens_charles_culliford_boz_1837;Charles Dickens;4;;;born in county",
+"dickens_mary_1838;Mary Dickens;3;;;born in county",
+"dickens_catherine_elizabeth_macready_1839;Catherine Dickens;1 1/2;;;born in county",
+"dickens_walter_savage_landor_1841;Walter Dickens;4mnths;;;born in county"
+],
+},
+
+
+"1851_dickens_charles_john_huffam_1812": {
+place: "34 Keppel St, Finsbury, London",
+gps: ["34 Keppel St, London https://maps.google.com/maps?q=51.520668,-0.129617"],
+images: ["Census record url:records/1851_dickens_charles_catherine_c1.jpg", "Census record url:records/1851_dickens_charles_catherine_c2.jpg"],
+source: "HO107; Piece: 1507; Folio: 206; Page: 16 ed06 http://search.ancestry.co.uk/cgi-bin/sse.dll?db=uki1851&indiv=try&h=2618450",
+cparish: "St George Bloomsbury, srd St George Bloomsbury, rd St Giles",
+
+head: "Robert Davey;Robert Davey;52;medical practitioner in surgery and ...;married;Beaston Litcham Norfolk",
+wife: "Eliza Davey;Eliza Davey;50;;married;London",
+serv: [
+"Rosanna Green;Rosanna Green;30;cook;single;Dublin Ireland",
+"Emma Riley;Emma Riley;24;house maid;single;London",
+"Frances Bousfield;Frances Bousfield;43;general maid;single;London"
+],
+visitors: [
+"barrow_elizabeth_culliford_1789;Elizabeth Dickens;60;annuitant;widow;London",
+"1851_dickens_charles_john_huffam_1812;Chas John Dickens;39;author;married;Portsmouth Hampshire",
+"dickens_alfred_allen_1813;Alfred Saml Dickens;29;engineer;married;Chatham Kent",
+"dickens_augustus_1827;Augustus Newsham Dickens;23;merchants clerk;married;London",
+"Henry Austin;Henry Austin;38;engineer;married;London",
+"dickens_letitia_1816;Letitia Austin;34;;married;London",
+"Elizabeth Austin;Elizabeth Austin;64;annuitant;widow;Birmingham",
+"Elizabeth Dorothy Smithson;Elizabeth Dorothy Smithson;40;annuitant;widow;Jamaica British subj",
+"Amelia Thompson;Amelia Thompson;42;annuitant;single;Jamaica British subj"
+],
+},
+
+
+"1861_dickens_charles_john_huffam_1812": {
+place: "3 Hanover Terrace, London",
+gps: ["3 Hanover Terrace, London https://maps.google.com/maps?q=51.527356,-0.162363"],
+images: ["Census record url:records/1861_dickens_charles_c.jpg"],
+source: "RG 9; Piece: 83; Folio: 111; Page: 1 ed16 http://search.ancestry.co.uk/cgi-bin/sse.dll?db=uki1861&indiv=try&h=8463678",
+cparish: "St Marylebone, srd Christchurch, rd St Marylebone",
+
+head: "dickens_charles_john_huffam_1812;Charles Dickens;49;author novelist essayist + editor;married;Hants Portsmouth",
+children: [
+"dickens_mary_1838;Mary Dickens;23;;single;Middlesex London",
+"dickens_francis_jeffrey_1844;Francis J Dickens;17;secretary;single;Middlesex London",
+"dickens_edward_bulwer_lytton_1852;Edward B Dickens;9;scholar;;Middlesex London"
+],
+serv: [
+"Georgina Hogarth;Georgina Hogarth;34;servant housekeeper;wifes sister, single;Scotland",
+"Susannah Cooper;Susannah Cooper;51;cook;mar;Kent Woolwich",
+"Matilda Bush;Matilda Bush;29;housemaid;single;Essex Walthamstow",
+"Agnes Garrod;Agnes Garrod;21;parlourmaid;single;Suffolk Bury St Edmunds"
+],
+},
+
+
+
+}
+
--- dickens/db.js
+++ dickens/db.js
@@ -4,14 +4,14 @@ barrow_elizabeth_culliford_1789:{
 g:"Elizabeth Culliford", k:"Elizabeth", f:"Barrow", bdate:"21 Dec", b:"1789", ddate:"13 Sep", d:"1863",
 bplace:"", dplace:"London", thumb:true, p:false,
 father:"Charles Barrow", mother:"Mary Culliford", occ:"", m:['Dickens'],
-fg:[["1836", "dickens_john_1785", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
+fg:[["1809-06-13", "dickens_john_1785", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
 },
 
 collins_charles_allston_1828:{
 g:"Charles Allston", k:"Charles", f:"Collins", bdate:"25 Jan", b:"1828", ddate:"9 Apr", d:"1873",
 bplace:"Hampstead", dplace:"London", thumb:true, male:true, p:false,
 father:"William Collins", mother:"", occ:"Artist, writer", cstatus:"No children",
-fg:[["1860", "dickens_catherine_elizabeth_macready_1839"]]
+fg:[["1860-07-01", "dickens_catherine_elizabeth_macready_1839"]],
 },
 
 dickens_alfred_allen_1813:{
@@ -43,23 +43,196 @@ dickens_catherine_elizabeth_macready_1839:{
 g:"Catherine Elizabeth Macready", k:"Kate", f:"Dickens", bdate:"29 Oct", b:"1839", ddate:"9 May", d:"1929",
 bplace:"", dplace:"", thumb:true, p:true,
 father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Painter", m:['Perugini'],
-fg:[["1860", "collins_charles_allston_1828"],
-    ["1874", "perugini_charles_edward_1839", "perugini_leonard_ralph_dickens_1876"]]
+fg:[["1860-07-01", "collins_charles_allston_1828"],
+    ["1874", "perugini_charles_edward_1839", "perugini_leonard_ralph_dickens_1876"]],
+
+marriages:{
+"collins_charles_allston_1828":{
+timestamp: "1860-07-01",
+groom: "Charles Allston Collins",
+gid: "collins_charles_allston_1828",
+bride: "Catherine Elizabeth Macready Dickens",
+bid: "dickens_catherine_elizabeth_macready_1839",
+}},
 },
+
 
 dickens_charles_culliford_boz_1837:{
 g:"Charles Culliford Boz", k:"Charley", f:"Dickens", bdate:"6 Jan", b:"1837", ddate:"20 Jul", d:"1896",
 bplace:"Holborn", dplace:"Fulham", thumb:true, male:true, p:false,
 father:"dickens_charles_john_huffam_1812", mother:"hogarth_catherine_thompson_1815", occ:"Writer, editor",
-fg:[["1861", "Elizabeth Matilda Moule Evans"]]
+fg:[["1861-07-01", "evans_elizabeth_matilda_moule_18xx"]],
+
+marriages:{
+"evans_elizabeth_matilda_moule_18xx":{
+timestamp: "1861-07-01",
+groom: "Charles Culliford Boz Dickens",
+gid: "dickens_charles_culliford_boz_1837",
+bride: "Elizabeth Matilda Moule Evans",
+bid: "evans_elizabeth_matilda_moule_18xx",
+}},
 },
+
+evans_elizabeth_matilda_moule_18xx:{
+g:"Elizabeth Matilda Moule", k:"", f:"Evans", bdate:"", b:"", ddate:"", d:"",
+bplace:"", dplace:"", thumb:true, p:false,
+father:"", mother:"", occ:"",
+fg:[["1861-07-01", "dickens_charles_culliford_boz_1837"]]
+},
+
+
 
 dickens_charles_john_huffam_1812:{
 g:"Charles John Huffam", k:"Charles", f:"Dickens", bdate:"7 Feb", b:"1812", ddate:"9 Jun", d:"1870",
 bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
 father:"dickens_john_1785", mother:"barrow_elizabeth_culliford_1789", occ:"Writer",
-fg:[["1836", "hogarth_catherine_thompson_1815", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845", "dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]]
+fg:[["1836", "hogarth_catherine_thompson_1815", "dickens_charles_culliford_boz_1837", "dickens_mary_1838", "dickens_catherine_elizabeth_macready_1839", "dickens_walter_savage_landor_1841", "dickens_francis_jeffrey_1844", "dickens_alfred_dorsay_tennison_1845", "dickens_sydney_smith_haldimand_1847", "dickens_henry_fielding_1849", "dickens_dora_annie_1850", "dickens_edward_bulwer_lytton_1852"]],
+
+intro:"Charles John Huffam Dickens was an English writer and social critic. He created some of the world's best-known fictional characters and is regarded by many as the greatest novelist of the Victorian era. His works enjoyed unprecedented popularity during his lifetime, and by the twentieth century critics and scholars had recognised him as a literary genius. His novels and short stories enjoy lasting popularity.",
+
+
+birth: {
+timestamp: "1812-03-04",
+place: "Landport, Hants",
+bapplace: "St Marys, Portsea, Hants",
+bapyear: "1812",
+bapdate: "4 Mar",
+gps: ["Portsea,Hampshire https://maps.google.com/maps?q=50.798778,-1.107263"],
+sources: `
+SOURCE: Select Births and Christenings, 1538-1975  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=FS1EnglandBirthsandChristenings&h=152229257
+Parish: St Marys, Portsea
+WhenBaptised: 4 Mar 1812
+Name: Charles John Huffham Dickens
+Father: John Dickens
+Mother: Elizabeth Dickens
+
+SOURCE: Wikipedia  https://en.wikipedia.org/wiki/Charles_Dickens`},
+
+marriages: {
+"hogarth_catherine_thompson_1815": {
+timestamp: "1836-04-02",
+year: "1836",
+date: "2 Apr",
+place: "St Lukes, Chelsea parish, London",
+groom: "Charles John Huffam Dickens",
+gid: "dickens_charles_john_huffam_1812",
+bride: "Catherine Thompson Hogarth",
+bid: "hogarth_catherine_thompson_1815",
+bage: "less than 21",
+gparish: "Furnival's Inn, London",
+bparish: "Chelsea parish",
+gstatus: "bachelor",
+bstatus: "spinster (minor)",
+bfather: "George Hogarth",
+witnesses: "George Hogarth(?), Elizabeth Dickens, others (writing illegible)",
+by: "licence",
+fnotes: ["The consent field on the marriage register says 'George Hogarth the ... and daughter(?) father(?) of the said ...'"],
+images: ["Parish record url:records/1836_dickens_charles_catherine_hogarth_m.jpg.jpg"],
+gps: ["St Lukes church, Chelsea, London https://maps.google.com/maps?q=51.489569,-0.169524"],
+sources: `
+SOURCE: Church of England Marriages and Banns, 1754-1921 http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=LMAmarriages&h=2187669
+Parish: St Lukes, Chelsea parish, London]
+Groom: Charles John Huffman Dickens
+GroomDetails: of Furnivals Inn, in this County, bachelor
+Bride: Catherine Thomson Hogarth
+BrideDetails: of this parish, spinster and minor
+By: licence
+When: 2 April 1836
+Witnesses: George Hogarth(?), Elizabeth Dickens, others (writing illegible)
+text: with Consent of George Hogarth the ... and daughter(?) father(?) of the said ...`}
 },
+
+death: {
+timestamp: "1870-06-09",
+of: "Gads Hill Place, near Rochester",
+age: "58",
+namedInProbate: "Georgina Hogarth and John Forster of Palace Gate House Kensington in the County of Middlesex Esquire the Executors",
+probate: `DICKENS Charles otherwise Charles John Huffham Esq. 19 July. The Will with a Codicil of Charles otherwise Charles John Huffham Dickens late of Gads Hill Place near Rochester in the County of Kent Esquire deceased who died 9 June 1870 at Gads Hill Place aforesaid was proved at the Principal Registry by the oaths of Georgina Hogarth and John Forster of Palace Gate House Kensington in the County of Middlesex Esquire the Executors. Effects under £80,000.`,
+images: ["Probate url:records/1870_dickens_charles_probate.jpg"],
+gps: ["Gads Hill Place, Higham, Kent https://maps.google.com/maps?q=51.411201,0.457370"],
+sources: `
+SOURCE: Civil Registration Death Index, 1837-1915  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=FreeBMDDeath&h=9292484
+Name: Charles Dickens
+Estimatedbirthyear: abt 1812
+RegistrationYear: 1870
+RegistrationQuarter: Apr-May-Jun
+AgeatDeath: 58
+Registrationdistrict: North Aylesford
+
+SOURCE: National Probate Calendar (Index of Wills and Administrations), 1858-1966, 1973-1995  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=UKProbateCal&h=5862786`},
+
+events: {
+"1824-07-01": {
+title: "Marshalsea debtor's prison",
+type: "note",
+notes: `
+note: The family had left Kent amidst rapidly mounting debts, and, living beyond his means, {dickens_john_1785:kf} was forced by his creditors into the Marshalsea debtors' prison in Southwark, London in 1824. His wife and youngest children joined him there, as was the practice at the time. {dickens_charles_john_huffam_1812:kx}, then 12 years old, boarded with Elizabeth Roylance, a family friend, at 112 College Place, Camden Town.
+note: On Sundays—with his sister {dickens_frances_elizabeth_1810:k}, free from her studies at the Royal Academy of Music—{dickens_charles_john_huffam_1812:kx} spent the day at the Marshalsea. He later used the prison as a setting in Little Dorrit. To pay for his board and to help his family, Dickens was forced to leave school and work ten-hour days at Warren's Blacking Warehouse, on Hungerford Stairs, near the present Charing Cross railway station, where he earned six shillings a week pasting labels on pots of boot blacking.`,
+place: "112 College Place, Camden Town",
+occ: "boot black labeller",
+sources: `
+SOURCE: Wikipedia  https://en.wikipedia.org/wiki/Charles_Dickens#Early_years`},
+
+
+"1824-07-02": {
+type: "figure",
+img: "pictures/marshalsea_prison_plan.jpg",
+caption: "Plan of the Marshalsea prison (in 1840s)",
+sources: `Wikipedia https://en.wikipedia.org/wiki/Marshalsea#/media/File:Plan_of_the_Marshalsea,_1843.jpg`
+},
+
+
+"1841-06-06": {
+type: 'census',
+census: '1841_dickens_charles_john_huffam_1812',
+relation: "head",
+occ: "Gentleman",
+notes: `{dickens_charles_john_huffam_1812:kx} was a gentleman. The children were {dickens_charles_culliford_boz_1837:k}, {dickens_mary_1838:k}, {dickens_catherine_elizabeth_macready_1839:k}, and {dickens_walter_savage_landor_1841:k} (aged 4 months).`,
+},
+
+"1841-07-01": {
+title: "Social setting",
+type: "note",
+notes: `At a time when Britain was the major economic and political power of the world, Dickens highlighted the life of the forgotten poor and disadvantaged within society. Through his journalism he campaigned on specific issues—such as sanitation and the workhouse—but his fiction probably demonstrated its greatest prowess in changing public opinion in regard to class inequalities. 
+He often depicted the exploitation and oppression of the poor and condemned the public officials and institutions that not only allowed such abuses to exist, but flourished as a result. His most strident indictment of this condition is in Hard Times (1854), Dickens's only novel-length treatment of the industrial working class. In this work, he uses vitriol and satire to illustrate how this marginalised social stratum was termed "Hands" by the factory owners; that is, not really "people" but rather only appendages of the machines they operated. His writings inspired others, in particular journalists and political figures, to address such problems of class oppression.`,
+sources: `SOURCE: Wikipedia  https://en.wikipedia.org/wiki/Charles_Dickens`},
+
+"1851-03-30": {
+type: 'census',
+census: '1851_dickens_charles_john_huffam_1812',
+relation: "visitor",
+occ: "Author",
+notes: `Robert Davey was a 52 year old medical practitioner.
+The visitors included {dickens_charles_john_huffam_1812:kx}'s mother, {barrow_elizabeth_culliford_1789:k},  widowed and living on an annuity, {dickens_charles_john_huffam_1812:kx}, described as an author, his brothers {dickens_alfred_allen_1813:k},  an engineer, and {dickens_augustus_1827:k},  a merchants clerk. Also there were his sister {dickens_letitia_1816:k},  her husband Henry Austin,  an engineer, and his mother Elizabeth Austin,  an annuitant. Finally there were two visitors born in Jamaica, Elizabeth Dorothy Smithson,  and Amelia Thompson,  both annuitants.
+The servants were Rosanna Green, 30, cook (born in Dublin), Emma Riley, 24, house maid, and Frances Bousfield, 43, a general maid.
+His father, {dickens_john_1785:kf}, is not listed at this address, but this is the abode given in his burial record. His date of death is given as the following day, so the family members were presumably gathered together on this account.`},
+
+"1861-04-07": {
+type: 'census',
+census: '1861_dickens_charles_john_huffam_1812',
+relation: "head",
+occ: "Author, novelist, essayist & editor",
+notes: `{dickens_charles_john_huffam_1812:kx} is described as an author, novelist, essayist & editor. His wife is not at the house because they had separated three years before.
+The children were {dickens_mary_1838:k},  {dickens_francis_jeffrey_1844:k},  a secretary, and {dickens_edward_bulwer_lytton_1852:k}, 9, at school.
+The servants included Georgina Hogarth, his wife's sister,  the housekeeper, and Susannah Cooper,  cook, Matilda Bush,  housemaid, and Agnes Garrod,  a parlour maid.`},
+
+"1865-06-09": {
+title: "Staplehurst train crash",
+type: "note",
+notes: `On 9 June 1865, while returning from Paris with Ellen Ternan, Dickens was involved in the Staplehurst rail crash. The train's first seven carriages plunged off a cast iron bridge that was under repair. The only first-class carriage to remain on the track was the one in which Dickens was travelling. Before rescuers arrived, Dickens tended and comforted the wounded and the dying with a flask of brandy and a hat refreshed with water, and saved some lives. Before leaving, he remembered the unfinished manuscript for Our Mutual Friend, and he returned to his carriage to retrieve it.`,
+sources: `SOURCE: Wikipedia  https://en.wikipedia.org/wiki/Charles_Dickens#Last_years`
+},
+
+
+
+"1865-07-02": {
+type: "figure",
+img: "pictures/staplehurst_rail_crash.jpg",
+caption: "Aftermath of the Staplehurst train crash",
+sources: `Wikipedia https://en.wikipedia.org/wiki/Charles_Dickens#/media/File:Staplehurst_rail_crash.jpg`
+},
+}},
+
 
 dickens_dora_annie_1850:{
 g:"Dora Annie", k:"", f:"Dickens", bdate:"16 Aug", b:"1850", ddate:"14 Apr", d:"1851",
@@ -108,7 +281,110 @@ dickens_john_1785:{
 g:"John", k:"", f:"Dickens", bdate:"21 Aug", b:"1785", ddate:"31 Mar", d:"1851",
 bplace:"Landport", dplace:"Higham", thumb:true, male:true, p:true,
 father:"William Dickens", mother:"Elizabeth Bal", occ:"Clerk",
-fg:[["1809", "barrow_elizabeth_culliford_1789", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]]
+fg:[["1809-06-13", "barrow_elizabeth_culliford_1789", "dickens_frances_elizabeth_1810", "dickens_charles_john_huffam_1812", "dickens_alfred_allen_1813", "dickens_letitia_1816", "dickens_harriet_1819", "dickens_frederick_1820", "dickens_alfred_lamert_1822", "dickens_augustus_1827"]],
+
+birth: {
+timestamp: "1785-08-21",
+bapplace: "St Marylebone, Middlesex",
+bapyear: "1785",
+bapdate: "20 Nov",
+images: ["Baptismal record url:records/1785_dickens_john_bap.jpg"],
+sources: `
+SOURCE: London Metropolitan Archives, St Marylebone, Westminster, Day book of baptisms, Jan 1779-Nov 1786, P89/MRY1/080  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=LMAearlyparish&h=343265
+text: Jno Dickens of Wm & Eliz b 21 Augt 85
+Born: 21 Aug 1785
+Name: Jno Dickens
+BaptismDate: 20 Nov 1785
+Parish: St Marylebone
+County: Middlesex
+Borough: Westminster
+Parent(s): Wm
+RecordType: Baptism
+RegisterType: Parish Register`},
+
+marriages: {
+"barrow_elizabeth_culliford_1789": {
+timestamp: "1809-06-13",
+year: "1809",
+date: "13 Jun",
+place: "St Mary Le Strand, Westminster",
+groom: "John Dickens",
+gid: "dickens_john_1785",
+bride: "Elizabeth Barrow",
+bid: "barrow_elizabeth_culliford_1789",
+bage: "<21",
+gparish: "St Mary Le Strand parish",
+bparish: "St Mary Le Strand parish",
+gstatus: "bachelor",
+bstatus: "spinster",
+witnesses: "Charles Barrow, Mary Barrow, Sarah Barrow",
+by: "licence",
+images: ["Parish record url:records/1809_dickens_john_barrow_elizabeth_m.jpg"],
+sources: `
+SOURCE: London Metropolitan Archives, Saint Mary Le Strand: Westminster, Transcript of Baptisms, Marriages and Burials, 1809 Mar-1810 Mar, DL/t Item, 097/009 http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=LMAmarriages&h=10103288
+Parish: St Mary Le Strand, Westminster]
+Groom: John Dickens
+GroomDetails: of this parish, bachelor
+Bride: Elizabeth Barrow
+BrideDetails: of this parish, spinster, minor
+By: licence
+When: 13 Jun 1809
+Signatures: John Dickens, Elizabeth Barrow
+Witnesses: Charles Barrow, Mary Barrow, Sarah Barrow
+text: by consent of her Father`}
+},
+
+death: {
+timestamp: "1851-03-31",
+of: "34 Keppel St, Russell Square",
+age: "66",
+buryear: "1851",
+burdate: "5 Apr",
+burplace: "St James, St Pancras, London",
+images: ["Burial record url:records/1851_dickens_john_bur.jpg"],
+gps: ["34 Keppel St, London https://maps.google.com/maps?q=51.520668,-0.129617"],
+notes: `Davey was John Dickens’s doctor; he had performed a painful operation on his patient’s bladder—without chloroformon 25 March in the Keppel Street house, and this was where John Dickens died, surrounded by family members, on the very night of the census (The Letters of Charles Dickens, vol. 6, 1988) [<a href="http://www.ucl.ac.uk/bloomsbury-project/articles/individuals/dickens_charles.htm">link</a>]`,
+sources: `
+SOURCE: Civil Registration Death Index, 1837-1915 1 46  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=FreeBMDDeath&h=9293977
+Name: John Dickens
+RegistrationYear: 1851
+RegistrationQuarter: Apr-May-Jun
+Registrationdistrict: St Giles
+
+SOURCE: London, England, Church of England Deaths and Burials, 1813-1980  http://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=try&db=LMAdeaths&h=10063916
+Abode: 34 Keppel St, Ansell Square
+Age: 66
+Name: John Dickens
+RecordType: Burial
+Age: 66
+Estimatedbirthyear: abt 1785
+DeathDate: abt 1851
+BurialDate: 5 Apr 1851
+BurialPlace: St James, St Pancras, England
+ParishorPoorLawUnion: St James, St Pancras
+Borough: Camden
+RegisterType: Bishop's Transcript`},
+
+events: {
+"1824-07-01": {
+title: "Marshalsea debtor's prison",
+type: "note",
+notes: `
+note: The family had left Kent amidst rapidly mounting debts, and, living beyond his means, {dickens_john_1785:kf} was forced by his creditors into the Marshalsea debtors' prison in Southwark, London in 1824. His wife and youngest children joined him there, as was the practice at the time. {dickens_charles_john_huffam_1812:kx}, then 12 years old, boarded with Elizabeth Roylance, a family friend, at 112 College Place, Camden Town.
+note: On Sundays—with his sister {dickens_frances_elizabeth_1810:k}, free from her studies at the Royal Academy of Music—{dickens_charles_john_huffam_1812:kx} spent the day at the Marshalsea. He later used the prison as a setting in Little Dorrit. To pay for his board and to help his family, Dickens was forced to leave school and work ten-hour days at Warren's Blacking Warehouse, on Hungerford Stairs, near the present Charing Cross railway station, where he earned six shillings a week pasting labels on pots of boot blacking.`,
+place: "112 College Place, Camden Town",
+occ: "boot black labeller",
+sources: `
+SOURCE: Wikipedia  https://en.wikipedia.org/wiki/Charles_Dickens#Early_years`},
+
+
+"1824-07-02": {
+type: "figure",
+img: "pictures/marshalsea_prison_plan.jpg",
+caption: "Plan of the Marshalsea prison (in 1840s)",
+sources: `Wikipedia https://en.wikipedia.org/wiki/Marshalsea#/media/File:Plan_of_the_Marshalsea,_1843.jpg`
+},
+}
 },
 
 dickens_letitia_1816:{
